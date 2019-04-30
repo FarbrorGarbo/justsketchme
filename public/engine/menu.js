bcc785e550a83c52d49b62167b82ab4d55aaab5b
Herman Martinus <hfbmartinus@gmail.com>
2019-04-30 17:00:54 +0000

    Lighting!

--- public/engine/menu.js
+++ public/engine/menu.js
@@ -2,9 +2,16 @@
 init();
 
 function init() {
-  const menu = document.querySelector('#info');
+  const menu = document.querySelector('.model-list');
   // menu.innerHTML = "";
   Models.forEach((model, index) => {
-    menu.innerHTML+= `<a href="javascript:loadModel(${index});">${model.name}</a><br>`;
+    // menu.innerHTML+= `<a href="javascript:loadModel(${index});">${model.name}</a><br>`;
+    menu.innerHTML += `
+    <li>
+      <button class="model" onclick="loadModel(${index});">
+        <img src="images/tools/stick-man.svg"/>
+        ${model.name}
+      </button
+    </li>`
   })
 }
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,20 +6,11 @@
              .register('./service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
-
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can add to home screen
-    btnAdd.style.display = 'block';
-  });
 })();
 
 
-function toggle_visibility(id) {
-  var e = document.getElementById(id);
+function toggle_visibility(query) {
+  var e = document.querySelector(query);
   if(e.style.display == 'block')
      e.style.display = 'none';
   else
